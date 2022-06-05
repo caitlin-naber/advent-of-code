71426a1fae5870733d50aae5a433a8890eb3b2ba
Caitlin Naber <caitlin.naber@gmail.com>
2022-06-05 19:40:29 -0400

    added puzzle 201501

--- com/puzzlerunners/Runner201501.java
+++ com/puzzlerunners/Runner201501.java
@@ -1,9 +1,50 @@
 package com.puzzlerunners;
 
-public class Runner201501 {
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
-    public int test(){
-        return 1;
+public class Runner201501 implements Runnable{
+
+    String inputData = "";
+
+    @Override
+    public void loadData() throws FileNotFoundException {
+        File inputFile = new File("data/Input201501.txt");
+        Scanner inputScanner = new Scanner(inputFile);
+        inputData = inputScanner.nextLine();
     }
 
+    @Override
+    public String solvePartOne() {
+        String[] data = inputData.split("");
+        int floor = 0;
+        for (String item : data) {
+            if (item.equals("(")) {
+                floor++;
+            } else {
+                floor--;
+            }
+        }
+        return String.valueOf(floor);
+    }
+
+    @Override
+    public String solvePartTwo() {
+        String[] data = inputData.split("");
+        int floor = 0;
+        int instruction = 1;
+        for (String item : data) {
+            if (item.equals("(")) {
+                floor++;
+            } else {
+                floor--;
+            }
+            if (floor == -1) {
+                return String.valueOf(instruction);
+            }
+            instruction++;
+        }
+        return "We never reached the basement!";
+    }
 }
