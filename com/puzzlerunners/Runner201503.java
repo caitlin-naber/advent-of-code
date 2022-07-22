922ccc70dbff53b68495b7eb54af073fbc1e7d23
Caitlin Naber <caitlin.naber@gmail.com>
2022-07-21 20:51:21 -0400

    solves puzzle for 2015 day 3

--- com/puzzlerunners/Runner201503.java
+++ com/puzzlerunners/Runner201503.java
@@ -18,24 +18,66 @@ public class Runner201503 implements Runnable {
 
     @Override
     public String solvePartOne() {
-        return null;
+        Set<List<Integer>> locations = new HashSet<>();
+        locations = deliverPresents(instructions);
+        int presentsDelivered = locations.size();
+        return String.valueOf(presentsDelivered);
     }
 
     @Override
     public String solvePartTwo() {
-        return null;
+        List<String> santaInstructions = selectInstructions(instructions, 0, 2);
+        List<String> roboSantaInstructions = selectInstructions(instructions, 1, 2);
+        Set<List<Integer>> santaLocations = new HashSet<>();
+        Set<List<Integer>> roboSantaLocations = new HashSet<>();
+        santaLocations = deliverPresents(santaInstructions);
+        roboSantaLocations = deliverPresents(roboSantaInstructions);
+        santaLocations.addAll(roboSantaLocations);
+        int presentsDelivered = santaLocations.size();
+        return String.valueOf(presentsDelivered);
     }
 
-    public int deliverPresents(List<String> instructions) {
+    public Set<List<Integer>> deliverPresents(List<String> instructions) {
         Set<List<Integer>> deliveryLocations = new HashSet<>();
+        int currentX = 0;
+        int currentY = 0;
         List<Integer> currentLocation = new ArrayList<>();
-        currentLocation.add(0);
-        currentLocation.add(0);
+        currentLocation.add(currentX);
+        currentLocation.add(currentY);
         deliveryLocations.add(currentLocation);
-        for (String instruction : instructions) {
 
+        for (String instruction : instructions) {
+            if (instruction.equals(">")) {
+                currentX++;
+            } else if (instruction.equals("<")) {
+                currentX--;
+            } else if (instruction.equals("v")) {
+                currentY--;
+            } else if (instruction.equals("^")) {
+                currentY++;
+            } else {
+                System.out.println("I'm lost!");
+            }
+            deliveryLocations = updateLocation(currentX, currentY, deliveryLocations);
         }
-        return 0;
+        return deliveryLocations;
+    }
+
+    public Set<List<Integer>> updateLocation(int x, int y, Set<List<Integer>> locations) {
+        List<Integer> currentLocation = new ArrayList<>();
+        currentLocation.add(x);
+        currentLocation.add(y);
+        locations.add(currentLocation);
+
+        return locations;
+    }
+
+    public List<String> selectInstructions(List<String> instructions, int start, int step) {
+        List<String> selectedInstructions = new ArrayList<>();
+        for (int i = start; i < instructions.size(); i = i + step) {
+            selectedInstructions.add(instructions.get(i));
+        }
+        return selectedInstructions;
     }
 
 
